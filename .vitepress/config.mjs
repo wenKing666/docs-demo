3eb70e86fe0966e6b60dc98c99f5d8e0cec19c8a
问荆 <1845035882@qq.com>
2025-03-14 16:38:02 +0800

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "WT的个人博客",
   description: "永远相信美好的事情即将发生",
-  base: "/docs-demo/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     logo:"/docs-demo/favicon.ico",
